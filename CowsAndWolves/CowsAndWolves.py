78bbdf5239354047768db98104dc04c8af6aa60c
HasanTahsinAytekin <76186086+HasanTahsinAytekin@users.noreply.github.com>
2021-01-21 14:51:10 +0300

    Add files via upload

--- CowsAndWolves/CowsAndWolves.py
+++ CowsAndWolves/CowsAndWolves.py
@@ -9,21 +9,22 @@ def plot_results_with_hyperplane(clf, clf_name, df, plt_nmbr):
     x_min, x_max = df.x.min() - .5, df.x.max() + .5
     y_min, y_max = df.y.min() - .5, df.y.max() + .5
 
-    # step between points. i.e. [0, 0.02, 0.04, ...]
+    # step between points.
     step = .02
-    # to plot the boundary, we're going to create a matrix of every possible point
-    # then label each point as a wolf or cow using our classifier
+    #
+    # Sınırı çizmek için olası her noktanın bir matrisini oluşturacağız. Sonra ilgili
+    #   sınıflandırıcıyı kullanarak her noktayı bir kurt yada ineğe bağlayacağız
     xx, yy = np.meshgrid(np.arange(x_min, x_max, step), np.arange(y_min, y_max, step))
     Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-    # this gets our predictions back into a matrix
+    # Tahminleri tekrar aynı matise yükleyelim
     Z = Z.reshape(xx.shape)
 
-    # create a subplot (we're going to have more than 1 plot on a given image)
+    # Bir subplot oluşturalım (her bir grafik üzerinde birden fazla yer olacak)
     pl.subplot(2, 2, plt_nmbr)
-    # plot the boundaries
+    # sınırları çiz
     pl.pcolormesh(xx, yy, Z, cmap=pl.cm.Paired)
 
-    # plot the wolves and cows
+    # Kurt ve inekleri çizelim
     for animal in df.animal.unique():
         pl.scatter(df[df.animal==animal].x,
                    df[df.animal==animal].y,
@@ -40,14 +41,14 @@ data = [row.split('\t') for row in data.strip().split('\n')]
 animals = []
 for y, row in enumerate(data):
     for x, item in enumerate(row):
-        # x's are cows, o's are wolves
+        # x'ler inek', o'lar kurt
         if item in ['o', 'x']:
             animals.append([x, y, item])
 
 df = pd.DataFrame(animals, columns=["x", "y", "animal"])
 df['animal_type'] = df.animal.apply(lambda x: 0 if x=="x" else 1)
 
-# train using the x and y position coordiantes
+# x ve y koordinatlarını kullanarak eğitelim
 train_cols = ["x", "y"]
 
 clfs = {
